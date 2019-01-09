a34771f7f6bd5d4b6f87cad7139654e2abb43e50
josharmantrout91 <josh.armantrout@gmail.com>
2019-01-09 17:24:47 -0500

    added Todo component and TodoList function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
+
+const todoData = [
+  { todoName: 'Fork and Clone' },
+  { todoName: 'Set up Branch' },
+  { todoName: 'React Wizardry' },
+  { todoName: 'Gaze at the Beauty of my Creation'}
+];
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      todoList: todoData
+    };
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-        <h2>Is This Thing On??</h2>
+      <div className ="App">
+        
       </div>
     );
   }
